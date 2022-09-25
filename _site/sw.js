aa8baa2d93d5dfe44f5059a642469449874baf38
8ku <8ku@8kudeMacBook-Pro.local>
2022-09-25 18:44:36 +0800

    fix mermaid

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/game/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220925.182352';
+const cacheName = 'chirpy-20220925.184403';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
