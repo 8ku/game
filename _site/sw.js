93877a71eb0b86cc8146003efbc8e9ec007faf45
8ku <8ku@8kudeMacBook-Pro.local>
2022-09-26 00:24:20 +0800

    update to master

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/game/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220926.001923';
+const cacheName = 'chirpy-20220926.002335';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
